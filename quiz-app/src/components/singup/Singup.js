5a7a2705e8c4b27f243f552432a901cfd07ff28b
yogeshkanthasamy <155651802+yogeshkanthasamy@users.noreply.github.com>
2024-05-17 19:26:59 +0530

    Update Singup.js

--- quiz-app/src/components/singup/Singup.js
+++ quiz-app/src/components/singup/Singup.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Singup() {
   return (
     <div>
-      Singup
+      <div>
+        <form>
+          <label>Name</label>
+          <input placeholder='Enter your name'></input><br></br>
+          <label>Email</label>
+          <input placeholder='Enter your email'></input><br></br>
+          <lable>Password</lable>
+          <input placeholder='Enter your password'></input><br></br>
+        </form>
+      </div>
     </div>
   )
 }
