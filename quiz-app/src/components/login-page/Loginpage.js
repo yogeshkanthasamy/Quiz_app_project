864603ef970c85e167485e0dfd223b751a49ec13
yogeshkanthasamy <155651802+yogeshkanthasamy@users.noreply.github.com>
2024-05-17 19:18:50 +0530

    Update Loginpage.js

--- quiz-app/src/components/login-page/Loginpage.js
+++ quiz-app/src/components/login-page/Loginpage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import { usercontext } from '../authentication/Auth'
+import './navbar.css'
 
-export default function Loginpage() {
+export default function Navbar() {
+  const loginuser=useContext(usercontext)
   return (
-    <div>
-      Loginpage
+    
+      <div>
+        <div className='container_nav'>
+          <div className='logo'> <a href='/'>PARTICE QUIZ?</a></div>
+      <NavLink to={"/"} className='navlink'>Home</NavLink>
+      
+      {loginuser.logged &&
+      <>
+      
+      <NavLink to={"/profile"} className='navlink'>profile</NavLink>
+      </>
+    }
+      <NavLink to={"/about"} className='navlink'>About</NavLink>
+      <NavLink to={"/singup"} className='navlink'>Singup</NavLink>
+      <NavLink to={"/login"} className='navlink'>Login</NavLink>
+
+      {/* <div><butto>mode</butto></div> */}
+
+      </div>
+      
     </div>
   )
 }
