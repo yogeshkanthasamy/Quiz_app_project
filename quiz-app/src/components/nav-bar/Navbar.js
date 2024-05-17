2ad762f1d1e0caeda29a9e7e5d662192f57ffc1e
yogeshkanthasamy <155651802+yogeshkanthasamy@users.noreply.github.com>
2024-05-17 17:20:19 +0530

    Update Navbar.js

--- quiz-app/src/components/nav-bar/Navbar.js
+++ quiz-app/src/components/nav-bar/Navbar.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { usercontext } from '../authentication/Auth'
+
 
 
 export default function Navbar() {
+  const loginuser=useContext(usercontext)
   return (
     <div>
+      <Navbar>
       <NavLink to={"/"}>Home</NavLink>
       <NavLink to={""}></NavLink>
+      {loginuser.logged &&
+      <>
+      
+      <NavLink to={"/profile"}>profile</NavLink>
+      </>
+    }
       <NavLink to={"/about"}>About</NavLink>
+      <NavLink to={"/singup"}>Singup</NavLink>
+      <NavLink to={"/login"}>Login</NavLink>
+      </Navbar>
     </div>
   )
 }
