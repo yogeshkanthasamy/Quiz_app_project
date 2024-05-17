5f6678b8ee5d7f193454f2f45d4872aabaa472c3
yogeshkanthasamy <155651802+yogeshkanthasamy@users.noreply.github.com>
2024-05-17 17:18:33 +0530

    Update index.js

--- quiz-app/src/index.js
+++ quiz-app/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Auth from './components/authentication/Auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Auth>
   <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Auth>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
