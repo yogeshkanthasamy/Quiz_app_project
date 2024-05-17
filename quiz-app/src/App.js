d9cc50dc02f280cc0db93c18cf3921b5381af46b
yogeshkanthasamy <155651802+yogeshkanthasamy@users.noreply.github.com>
2024-05-17 17:17:58 +0530

    Update App.js

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/home-page/Home';
 import Navbar from './components/nav-bar/Navbar';
 import About from './components/about-page/About';
+import Loginpage from './components/login-page/Loginpage';
+import Singup from './components/singup/Singup';
+import Userprofile from './components/profile_page/Userprofile';
+import Logout from './components/logout/Logout';
+
 
 function App() {
   return (
@@ -12,9 +17,14 @@ function App() {
      <Routes>
      <Route path='/' element={<Home/>}></Route>
      <Route path='/about' element={<About/>}></Route>
+     <Route path='/singup'element={<Singup/>}></Route>
+     <Route path='/login'element={<Loginpage/>}></Route>
+     <Route path='/profile'element={<Userprofile/>}></Route>
+     
      </Routes>
     </div>
   );
 }
 
 export default App;
+
